2e32a9821d95d327db9f48880388a248e8ac7bbc
Chris Adkin <chris.adkin@hashicorp.com>
2024-09-11 14:44:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@
 
 resource "terraform_data" "test" {
   provisioner "local-exec" {
-    command = "jq ' .Modules[] | { module: .Key, version: .Version  }'  .terraform/modules/modules.json "
+    command = "jq ' .Modules[] | { module: .Key, version: .Version  }'  .terraform/modules/modules.json > module_versions.txt"
   }
 }
 
-data "local_file" "modules_json" {
-  filename = "${path.cwd}/.terraform/modules/modules.json"
+data "local_file" "example_output" {
+  filename = "${path.module}/module_versions.txt"
 }
