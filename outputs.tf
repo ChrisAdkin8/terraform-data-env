da4fd11d3396dbaa251d45210573bc881d8477d6
Chris Adkin <chris.adkin@hashicorp.com>
2024-09-11 11:20:55 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,12 @@
 # ------------------------------------------------------------------------------
 # Output
 # ------------------------------------------------------------------------------
-output "environment_data_all" {
-  value       = data.environment_variables.all
-  description = "All environment variables, derived from Epp0/environment provider"
-}
+#output "environment_data_all" {
+#  value       = data.environment_variables.all
+#  description = "All environment variables, derived from Epp0/environment provider"
+#}
 
 output "modules_json" {
   value       = jsondecode(data.local_file.modules_json.content)
   description = "The content of the modules.json file"
 }
-
