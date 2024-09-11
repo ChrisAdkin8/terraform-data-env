ccbe06a980fbfdacd66f61c6e4180f1d10b7b958
Chris Adkin <chris.adkin@hashicorp.com>
2024-09-11 14:57:44 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@
 # Output
 # ------------------------------------------------------------------------------
 output "modules_json" {
-  value       = jsondecode(value = data.local_file.module_versions.content)
+  value       = jsondecode(data.local_file.module_versions.content)
   description = "The content of the modules.json file"
 }
