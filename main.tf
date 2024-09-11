23fb41e3a26c186d8e4c37dde4bcbf25aaaf378e
Chris Adkin <chris.adkin@hashicorp.com>
2024-09-11 11:35:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,20 +5,19 @@
 
 resource "terraform_data" "test" {
   # This resource exists purely for spelunking the TFC/E workspace
-  provisioner "local-exec" {
-    command = "ls -lia"
-  }
-  provisioner "local-exec" {
-    command = "pwd"
-  }
-  provisioner "local-exec" {
-    command = "cat ${path.cwd}/.terraform/modules/modules.json | jq . "
-  }
+  # provisioner "local-exec" {
+  #  command = "ls -lia"
+  #}
+  #provisioner "local-exec" {
+  #  command = "pwd"
+  #}
+  #provisioner "local-exec" {
+  #  command = "cat ${path.cwd}/.terraform/modules/modules.json | jq . "
+  #}
   provisioner "local-exec" {
     command = "jq ' .Modules[] | { module: .Key, version: .Version  }'  .terraform/modules/modules.json "
   }
 }
-
 
 data "local_file" "modules_json" {
   filename = "${path.cwd}/.terraform/modules/modules.json"
